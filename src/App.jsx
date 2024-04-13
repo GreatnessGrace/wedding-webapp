61b2aa11d708cf8a5b0d00cb89e6d56a4e29e9c5
GreatnessGrace <graceunstoppable7@gmail.com>
2024-04-13 22:09:39 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+import Example from "./components/HorizontalCarousal";
+import { ParallaxScrollSecondDemo } from "./components/Demo";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1> 
-  )
-}
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        {/* <Collaboration /> */}
+        <Services />
+        <ParallaxScrollSecondDemo/>
+        {/* <Example/> */}
+        {/* <Pricing /> */}
+        {/* <Roadmap /> */}
+        <Footer />
+      </div>
 
-export default App
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
